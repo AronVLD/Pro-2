d4690a389b230a7363db62506bbf71e236cbd6b4
Aron van de Ridder <aronridder@hotmail.nl>
2019-12-18 09:13:37 +0100

    added some things to opleidingen

--- opleiding.php
+++ opleiding.php
@@ -20,15 +20,30 @@
   <div class="container-fluid">
   <div class="row">
   <div class="jumbotron col-6">
-  <h1 class="display-4">informatie</h1>
-  <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, quam? Laborum doloribus praesentium, tempore corrupti dolorum quia eius officia voluptatum possimus sit ipsam expedita, dignissimos, odit error omnis architecto suscipit.</p>
+  <h1 class="display-4">Astronaut worden?!</h1>
+  <p class="lead">
+  <img src="img\Become_an_astronaut_-_NL.jpg" alt="Meld je aan bij "aanmelden" in de navbar!">
+  </p>
   <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p></div>
+  <p>Als jij astronaut wilt worden, Meld je dan nu aan op onze website!</p></div>
   <div class="jumbotron col-6">
-  <h1 class="display-4">dingen</h1>
-  <p class="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis omnis, consectetur quam quod est commodi deserunt eos recusandae voluptates aut. Tempore, totam officiis vero delectus quo id quisquam non libero.</p>
+  <h1 class="display-4">Strenge eisen</h1>
+  <p class="lead">
+
+jullie moeten in goede gezondheid zijn, goed kunnen zien en een normale bloeddruk hebben.<br>
+<br>
+Ook is het handig als jullie een lichaamslengte tussen de 1.70 en 1.80 meter hebben.<br>
+<br>
+Een technische opleiding aan de universiteit is een voorwaarde, inclusief onderzoek naar op het gebied van ruimtevaart.<br>
+<br>
+De sollicitatie van de NASA en de Europese variant ESA is geen kwestie van een paar gesprekjes: je moet uitgebreide vragenlijsten invullen, conditietesten doInstromen via Defensie kan ook, bijvoorbeeld als je piloot wilt worden.<br>
+en en helemaal gezond zijn verklaard.<br> 
+<br> 
+Pas als je door alle tests bent gekomen, mag je trainingen gaan volgen voor een eventuele ruimtereis. Maar zelfs een perfecte gezondheid garandeert geen probleemloze reis. "Er zijn astronauten die aan alle eisen voldoen maar het toch niet trekken, lichamelijk of geestelijk", aldus Van der Lande, die ook heimwee en ruzies aan boord als mogelijke obstakels noemt. "Je zou miljonair kunnen worden als je een test uitvindt die alle twijfels weghaalt, want uitsluitsel geeft de training nog niet." 
+</p>
   <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p></div>
+  </div>
+  
 
 </div>
 </div>
